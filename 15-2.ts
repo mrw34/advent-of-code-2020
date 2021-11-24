2edeae1fd1b230b0845bba9c7094e9bc7b85e38c
Mark Woodbridge <1101318+mrw34@users.noreply.github.com>
2021-11-24 18:38:41 +0000

    15-2

---
+++ 15-2.ts
@@ -0,0 +1,21 @@
+import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
+
+const $ = Deno.readTextFileSync("15.txt").trimRight().split(",").map(Number);
+
+let current, previous: number | undefined;
+for (let i = 0, a = []; i < 30000000; i++) {
+  if (i < $.length) {
+    current = $[i];
+  } else {
+    if (a[previous!] === undefined) {
+      current = 0;
+    } else {
+      current = (i - 1) - a[previous!];
+    }
+  }
+  if (previous !== undefined) {
+    a[previous] = i - 1;
+  }
+  previous = current;
+}
+assertEquals(current, 63644);
